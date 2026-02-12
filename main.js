d7533e6c7dbef4148e0f783c282cd9033d8c316a
yahya ahmed <yahyabar2007@gmail.com>
2026-02-12 16:52:29 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+let attempets = 0;
+let score = 0;
+function guess(userGuess) {
+    attempets++;
+    document.getElementById("attempts").innerHTML = attempets;
+    //randomNumber
+    const randomNumber = Math.floor(Math.random() * 100) + 1;
+    //correctAnswer
+    const correctAnswer = randomNumber % 2 == 0 ? "even" : "odd";
+    //SFXsound
+    function sfx(isCorrect) {
+        let audio = isCorrect
+            ? document.getElementById("correct")
+            : document.getElementById("wrong");
+        audio.play();
+        audio.currntTime = 0;
+        audio.puase();
+    }
+    if (correctAnswer === userGuess) {
+        //update display
+        document.getElementById("display").innerHTML = "correct answer is " + randomNumber;
+        //increase score
+        score++;
+        document.getElementById("Score").innerHTML = score;
+        //sound
+        sfx(true);
+    } else {
+        //update display
+        document.getElementById("display").innerHTML = "false answer  "
+            + randomNumber;
+        //sound
+        sfx(false);
+    }
+
+}
